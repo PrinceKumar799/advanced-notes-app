2e33f83d9a56a84d730c8204b9ebb24754994709
Prince Kumar <singhprince799@outlook.com>
2023-08-15 23:36:09 +0530

    new note creation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useMemo, useRef } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Container } from "react-bootstrap";
+import { Navigate, Route, Routes } from "react-router-dom";
+import NewNote from "./NewNote";
+import ShowNotes from "./ShowNotes";
+import useLocalStorage from "./useLocalStorage";
+import { v4 as uuidV4 } from "uuid";
+import NoteCard from "./NoteCard";
+import HomePage from "./HomePage";
 function App() {
+  const [notes, setNotes] = useLocalStorage("NOTES", []);
+  const [tags, setTags] = useLocalStorage("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+  const onCreateNotes = ({ tags, ...data }) => {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        {
+          ...data,
+          id: uuidV4(),
+          tagIds: tags.map((tag) => tag.id),
+        },
+      ];
+    });
+  };
+  const addTag = (newTag) => {
+    setTags((prev) => [...prev, newTag]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="my-4">
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage availableTags={tags} notes={notesWithTags} />}
+        />
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={onCreateNotes}
+              onAddTag={addTag}
+              availableTags={tags}
+            />
+          }
+        />
+        <Route path="/:id">
+          <Route index element={<ShowNotes />} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   );
 }
 
