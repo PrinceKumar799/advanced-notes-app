c19ba6cbadb2ed94f6242260738c14fdc4307b1b
Prince Kumar <singhprince799@outlook.com>
2023-08-17 00:18:15 +0530

    read notes functionality

--- src/ShowNotes.js
+++ src/ShowNotes.js
@@ -1,8 +1,37 @@
-export default function ShowNotes({ data }) {
+import { useOutletContext } from "react-router-dom";
+import { Row, Stack, Badge, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+export default function ShowNotes() {
+  const note = useOutletContext();
   return (
-    <div>
-      <h1>{data.title}</h1>
-      <p>{data.text}</p>
-    </div>
+    <>
+      <Row className="align-items-center mb-4">
+        <Col>
+          <h1>{note.data.title}</h1>
+          {note.tags.length > 0 && (
+            <Stack gap={1} className="flex-wrap" direction="horizontal">
+              {note.tags.map((tag) => (
+                <Badge key={tag.id} className="text-truncate">
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Col>
+        <Col xs="auto">
+          <Stack direction="horizontal" gap={2}>
+            <Link to={`/${note.id}/edit`}>
+              <Button variant="primary">Edit</Button>
+            </Link>
+            <Button variant="outline-danger">Delete</Button>
+            <Link to={`/`}>
+              <Button variant="outline-secondary">Back</Button>
+            </Link>
+          </Stack>
+        </Col>
+      </Row>
+      <ReactMarkdown>{note.data.text}</ReactMarkdown>
+    </>
   );
 }
